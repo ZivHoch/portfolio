941ffd9ed7a1b5d8834eba61c12dd4f320fd37f4
Zivho <ziv.h252@gmail.com>
2025-05-20 00:12:14 +0200

    use an api

--- frontend/src/services/github.js
+++ frontend/src/services/github.js
@@ -2,12 +2,7 @@ const GITHUB_API_BASE = "https://api.github.com";
 const GITHUB_AUTH = import.meta.env.VITE_GITHUB_TOKEN;
 export const fetchUserRepos = async (username) => {
   try {
-    const response = await fetch(`${GITHUB_API_BASE}/users/${username}/repos`, {
-      headers: {
-        Authorization: `token ${GITHUB_AUTH}`,
-        Accept: "application/vnd.github.v3+json",
-      },
-    });
+    const response = await fetch(`${username}:${GITHUB_AUTH} ${GITHUB_API_BASE}/users/${username}/repos`);
     if (!response.ok) throw new Error("Failed to fetch repos");
     return await response.json();
   } catch (error) {
