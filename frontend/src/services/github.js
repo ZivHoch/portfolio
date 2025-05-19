26ac1e484d075a0c2902f398208912ca3cb6da2d
Zivho <ziv.h252@gmail.com>
2025-05-20 00:01:07 +0200

    updating the permissions of the token

--- frontend/src/services/github.js
+++ frontend/src/services/github.js
@@ -1,8 +1,13 @@
 const GITHUB_API_BASE = "https://api.github.com";
-
+const GITHUB_AUTH = import.meta.env.VITE_GITHUB_TOKEN;
 export const fetchUserRepos = async (username) => {
   try {
-    const response = await fetch(`${GITHUB_API_BASE}/users/${username}/repos`);
+    const response = await fetch(`${GITHUB_API_BASE}/users/${username}/repos`, {
+      headers: {
+        Authorization: `token ${GITHUB_AUTH}`,
+        Accept: "application/vnd.github.v3+json",
+      },
+    });
     if (!response.ok) throw new Error("Failed to fetch repos");
     return await response.json();
   } catch (error) {
